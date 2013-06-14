b397041b9a22567a6fcdbf592ee7ce1d76562601
tzechienchu <tzechienchu@gmail.com>
2013-06-14 10:46:05 +0800

    Output a json format data

--- digitalnurserysensor.ino
+++ digitalnurserysensor.ino
@@ -12,6 +12,11 @@
 #include <Wire.h>
 //CO2
 #define CO2Pin A0
+//Light Sensor I2C
+#include <Digital_Light_TSL2561.h>
+
+#define QUOTE Serial.write(34)
+#define TABSIZE 8
 
 float Stemp_c;
 float Stemp_f;
@@ -23,7 +28,8 @@ float Bpressure;
 float Batm;
 float Baltitude;
 int   CO2Level;
-  
+unsigned long  Lux;
+
 Barometer myBarometer;
 DHT dht(DHTPIN, DHTTYPE);
 SHT1x sht1x(dataPin, clockPin);
@@ -31,14 +37,76 @@ SHT1x sht1x(dataPin, clockPin);
 
 void setup()
 {
-   Serial.begin(38400); // Open serial connection to report values to host
+   Wire.begin(); 
+   Serial.begin(115200); // Open serial connection to report values to host
    Serial.println("Starting up");
+   
    myBarometer.init();
+   TSL2561.init(); 
+   
 }
 
 void loop()
 {
 
+  readSensor()
+  jsonOut();
+  
+  delay(2000);
+}
+void showIndent(int indent)
+{
+  for(int i=0;i<indent*TABSIZE;i++) Serial.print(" ");
+}
+void jsonSectionOut(int indent,char *key)
+{
+  showIndent(indent);
+  QUOTE;Serial.print(key);QUOTE;Serial.println(": {"); 
+}
+void jsonSectionEndOut(int indent,int final)
+{
+  showIndent(indent);
+  Serial.print("}"); 
+  if (final == 0) {
+    Serial.println(",");
+  } else {
+    Serial.println();
+  }
+}
+void jsonKVOut(int indent,char *key,float value,int final)
+{
+  showIndent(indent);
+  QUOTE;Serial.print(key);QUOTE;Serial.print(": "); 
+  Serial.print(value, DEC);
+  if (final == 0) {
+    Serial.println(",");
+  } else {
+    Serial.println();
+  }
+}
+void jsonOut()
+{
+  Serial.print("{");QUOTE;Serial.print("DNSensor");QUOTE;Serial.println(": {");
+    jsonSectionOut(1,"Soil");
+      jsonKVOut(2,"Temperature",Stemp_c,0);
+      jsonKVOut(2,"Humidity",Shumidity,1);
+    jsonSectionEndOut(1,0);
+    jsonSectionOut(1,"Air");
+      jsonKVOut(2,"Temperature",Atemp_c,0);
+      jsonKVOut(2,"Humidity",Ahumidity,1);
+    jsonSectionEndOut(1,0);
+    jsonSectionOut(1,"Barometer");
+      jsonKVOut(2,"Temperature",Btemperature,0);
+      jsonKVOut(2,"Pressure",Bpressure,1);
+    jsonSectionEndOut(1,0);
+    jsonSectionOut(1,"Other");
+      jsonKVOut(2,"CO2",CO2Level,0);
+      jsonKVOut(2,"Lux",Lux,1);
+    jsonSectionEndOut(1,1);    
+  Serial.println("}}");
+}
+void readSensor()
+{
   // Read values from the sensor
   Stemp_c = sht1x.readTemperatureC();
   Stemp_f = sht1x.readTemperatureF();
@@ -53,12 +121,9 @@ void loop()
   Batm = Bpressure / 101325;  
   
   CO2Level = analogRead(CO2Pin);   
-  
-  debugOut();
-  
-  delay(2000);
+  TSL2561.getLux();
+  Lux=TSL2561.calculateLux(0,0,1);
 }
-
 void debugOut()
 {
   // Print the values to the serial port
@@ -92,6 +157,8 @@ void debugOut()
   Serial.print("CO2 = " );                       
   Serial.print(CO2Level);     
   Serial.println();
+  Serial.print("Lux = ");
+  Serial.println(Lux);
   
   Serial.println();
 }
